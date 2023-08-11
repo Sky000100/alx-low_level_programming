abacbc8477b923a2a97139980dd155cdd88a4e27
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-08-11 14:46:28 -0700

    update file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,11 +4,12 @@
  * main - Entry point
  *
  * This program prints all lowercase alphabet letters
- * except for 'e', 'i', and 'o', followed by a newline.
+ * 
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
@@ -21,5 +22,5 @@ int main(void) {
 
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
