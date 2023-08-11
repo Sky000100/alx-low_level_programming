056db2b16def3d1e2995b32f4011cac87fca6405
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-08-11 16:07:25 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints all possible combinations of single-digit numbers.
  *
- * This program generates and prints all possible combinations of two-digit
- * numbers (00 to 99), separated by commas and spaces, using the putchar
- * function exactly four times.
- *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	int tens_digit, ones_digit;
+	int num;
 
-	for (tens_digit = 0; tens_digit < 10; tens_digit++) 
+	for (num = 0; num <= 9; num++)
 	{
-		for (ones_digit = 0; ones_digit < 10; ones_digit++)
-		{
-		putchar(tens_digit + '0');
-		putchar(ones_digit + '0');
-
-		if (tens_digit < 9 || ones_digit < 9)
-			{
-                	putchar(',');
-                	putchar(' ');
-			}
-		}
+		putchar((num % 10) + '0');
+		if (num == 9)
+		continue;
+		
+		putchar(',');
+		putchar(' ');
 	}
 
 	putchar('\n');
