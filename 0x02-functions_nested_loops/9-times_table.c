b4a0e23258287451d9613ef4e9365650219604ca
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-08-15 17:42:00 -0700

    update file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,36 +5,29 @@
  */
 void times_table(void)
 {
-	int row, column, result;
+	int num, mult, prod;
 
-	for (row = 0; row <= 9; row++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (column = 0; column <= 9; column++)
-	{
-		result = row * column;
+		_putchar(48);
 
-		if (column != 0)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
-		if (result < 10)
-			_putchar(' ');
-
-		_putchar(' ');
+			prod = num * mult;
+			/**
+			 *	put space if product is a single number
+			 *	place the first digit if its two numbers
+			*/
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48)
 		}
-
-		if (result >= 10)
-		{
-			_putchar((result / 10) + '0');
-			_putchar((result % 10) + '0');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar(result + '0');
-		}
-	}
 		_putchar('\n');
 	}
+
 }
