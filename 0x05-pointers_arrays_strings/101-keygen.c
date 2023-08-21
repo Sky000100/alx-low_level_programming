971fa1cbc2626c52dc650391e225c38dc45b313a
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-08-21 10:33:49 -0700

    Don not hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * random_char - Generates a random character between min and max.
+ * @min: The minimum character value.
+ * @max: The maximum character value.
+ * Return: The generated random character.
+ */
+char random_char(char min, char max)
+{
+	return (min + rand() % (max - min + 1));
+}
+
+/**
+ * generate_password - Generates a random password of a given length.
+ * @length: The desired length of the password.
+ */
+void generate_password(int length)
+{
+	srand(time(NULL));
+
+	char lowercase_letters[] = "abcdefghijklmnopqrstuvwxyz";
+	char uppercase_letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char digits[] = "0123456789";
+	char special_characters[] = "!@#$%^&*";
+
+	char password[length + 1];
+	int charset_size, charset_choice;
+
+	for (int i = 0; i < length; i++)
+	{
+	charset_choice = rand() % 4;
+
+	if (charset_choice == 0)
+		charset_size = sizeof(lowercase_letters) - 1;
+	else if (charset_choice == 1)
+		charset_size = sizeof(uppercase_letters) - 1;
+	else if (charset_choice == 2)
+		charset_size = sizeof(digits) - 1;
+	else
+		charset_size = sizeof(special_characters) - 1;
+
+	password[i] = random_char(lowercase_letters[0],
+			lowercase_letters[charset_size - 1]);
+	}
+
+	password[length] = '\0';
+
+	printf("Generated password: %s\n", password);
+}
+
+/**
+ * main - Entry point of the program.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int password_length = 12;
+
+	generate_password(password_length);
+
+	return (0);
+}
+
