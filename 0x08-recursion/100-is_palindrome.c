f055a3dee7dd4f859090190115fa677917879e0c
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-08-31 12:25:08 -0700

    update file

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,26 +1,51 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The string to check.
+ * last_index - return the last index of a string (counts the null char)
+ * @s: pointer the string
+ * Return: int
+ */
+
+int is_palindrome(char *s);
+int check(char *s, int start, int end, int mod);
+int last_index(char *s)
+{
+	int n = 0;
+
+	if (*s > '\0')
+		n += last_index(s + 1) + 1;
+	return (n);
+}
+
+/**
+ * is_palindrome- Checks if a string is a palindrome
+ * @s: The string to check
  *
- * Return: 1 if the string is a palindrome, 0 if it's not.
+ * Return: 1 if the string is a palindrome, 0 if it's not
  */
 int is_palindrome(char *s)
 {
-	int length = strlen(s);
-	int start = 0;
-	int end = length - 1;
+	int end = last_index(s);
 
-	while (start < end)
-	{
-		if (s[start] != s[end])
-			return (0);
+	return (check(s, 0, end - 1, end % 2));
+}
 
-		start++;
-		end--;
-	}
+/**
+ * check - checker for the palindrome
+ * @s: string
+ * @start: int moves from right to left
+ * @end: int moves fomr left to right
+ * @mod: int
+ * Return: 0 or 1
+ */
 
-	return (1);
+int check(char *s, int start, int end, int mod)
+{
+	if ((start == end && mod != 0) || (start == end + 1 && mod == 0))
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (check(s, start + 1, end - 1, mod));
+
 }
