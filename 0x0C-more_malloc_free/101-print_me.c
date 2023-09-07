577454dfd6b7cfeac03dfc8d9acf6068f6d7f0bf
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-09-07 12:42:34 -0700

    task 101

---
+++ 0x0C-more_malloc_free/101-print_me.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_me - prints my array of the hopeful product here
+ * @sum_result: pointer to int array with numbers to add
+ * @len_r: length of result array
+ * Return: void
+ */
+void print_me(int *sum_result, int len_r)
+{
+	int i = 0;
+
+	while (sum_result[i] == 0 && i < len_r)
+		i++;
+	if (i == len_r)
+		_putchar('0');
+	while (i < len_r)
+		_putchar(sum_result[i++] + '0');
+	_putchar('\n');
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,9 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+void print_me(int *sum_result, int len_r);
+int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r);
+int str_len(char *str);
 
 #endif
 
