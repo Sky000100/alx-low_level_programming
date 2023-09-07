ea710945fbcc06840a9f0f7504e0f7ee2e86060f
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-09-07 11:36:17 -0700

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,39 +22,27 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+	void *p;
 	unsigned int i;
-
-	if (new_size == old_size)
-		return (ptr);
 
 	if (ptr == NULL)
 	{
-		if (new_size == 0)
-			return (NULL);
-
-		return (malloc(new_size));
+		p = malloc(new_size);
+		return (p);
 	}
-
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	new_ptr = malloc(new_size);
-
-	if (new_ptr == NULL)
+	if (old_size == new_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (p == NULL)
 		return (NULL);
-
-	if (new_size > old_size)
-		new_size = old_size;
-
-	for (i = 0; i < new_size; i++)
-		((char *)new_ptr)[i] = ((char *)ptr)[i];
-
+	for (i = 0; i < old_size && i < new_size; i++)
+		((char *)p)[i] = ((char *)ptr)[i];
 	free(ptr);
-
-	return (new_ptr);
+	return (p);
 }
 
