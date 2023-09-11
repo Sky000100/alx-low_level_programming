411222d3fff15d6fe7c7b7b2ab6f4a797f069e1d
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-09-11 13:11:29 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This program prints the name of the file it was compiled from.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("This program was compiled from the file: %s\n", __FILE__);
+	return (0);
+}
+
