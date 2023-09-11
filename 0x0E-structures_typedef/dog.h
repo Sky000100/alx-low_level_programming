0ee6576a76542f95a944635b358b48d64db17e02
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-09-11 13:49:08 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,17 +11,12 @@
  * Description: This structure defines the elements representing a dog's
  * information, including its name, age, and owner.
  */
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
 
