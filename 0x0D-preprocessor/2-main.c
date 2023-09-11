0f9c928a81f0d6cf54a7415965ae196090514552
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-09-11 13:23:41 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-
-#define PRINT_FILE_NAME \
-	printf("This program was compiled from the file: %s\n", __FILE__)
-
 /**
- * main - Entry point of the program.
- *
- * Description: This program prints the name of the file it was compiled from.
- *
- * Return: Always 0.
+ * main - prints the name of file
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	PRINT_FILE_NAME;
+	printf("%s\n", __FILE__);
 	return (0);
 }
-
