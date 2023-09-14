010ef9d0cf4ff3e4f55caadda63f221868192c31
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-09-14 10:14:11 -0700

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Returns the sum of its parameters.
+ * @n: The number of arguments.
+ * @...: The variable arguments.
+ *
+ * Return: The sum of the arguments.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return (sum);
+}
+
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,15 @@
+0x10. C - Variadic functions
+
+Autho: Mohammed Faisal
+
+Sep 14, 2023 7:00 AM
+
+Tasks:
+
+0-sum_them_all.c
+
+1-print_numbers.c
+
+2-print_strings.c
+
+3-print_all.c
