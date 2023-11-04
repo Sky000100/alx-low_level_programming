edc16278d7b336d81a17e1310234d206c5b8a338
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-11-04 00:17:53 -0700

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,13 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include "main.h"
 /**
- * read_textfile - Reads a text file and prints
- *		it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it
  * @filename: The name of the file to read.
- * @letters: The number of letters it should read and print.
+ * @letters: The number of bytes it should read.
  *
  * Return: The actual number of letters it could read and print,
  * or 0 on failure.
@@ -24,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	bytes = read(fd. &buf[0], letters);
+	bytes = read(fd, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
 	return (bytes);
