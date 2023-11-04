4546812d1213c13f343a8d093ff12dd855aba7ae
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-11-04 01:29:20 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #define USAGE "Usage: cp file_from file_to\n"
 #define ERR_NOREAD "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
-#define ERR_NOCLOSE "Error: Can't close fd %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
@@ -14,7 +14,7 @@
  * Return: 1 on success 0 on failure
  */
 
-int main(int argc, char *argv[])
+int main(int ac, char **av)
 {
 	int from_fd = 0, to_fd = 0;
 	ssize_t b;
@@ -24,12 +24,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, USAGE), exit(97);
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, av[1], exit(98);
-	to_fd = open(ac[2], O_CREAT | O_WRONLY | O_TRUNC, PERMISSIONS);
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2], exit(99);
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	while ((b = read(from_fd, buff, READ_BUF_SIZE)) > 0)
+	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
 		if (write(to_fd, buf, b) != b)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 	to_fd = close(to_fd);
 	if (from_fd)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
-	if (fd_to_fd)
+	if (to_fd)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 
 	return (EXIT_SUCCESS);
