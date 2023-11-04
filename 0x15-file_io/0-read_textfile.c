158f16a3aea53c9c907a444f509c300e7c513198
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-11-04 00:00:39 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,33 +15,18 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buffer[1024]; /* A buffer to read and write data */
-	ssize_t bytes_read, bytes_written, total_bytes_written = 0;
+	char buf[READ_BUF_SIZE * 8];
+	ssize_t bytes;
 
-	if (filename == NULL)
+	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
-	{
-		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			close(fd);
-			return (0);
-		}
-		total_bytes_written += bytes_written;
-		if (total_bytes_written >= (ssize_t)letters)
-			break;
-	}
-
+	bytes = read(fd. &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
-
-	if (bytes_read == -1)
-		return (0);
-
-	return (total_bytes_written);
+	return (bytes);
 }
 
